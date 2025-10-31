ea91276ae6071f00017ee1c659e467ce8016822c
Masanori-Satoh <duckgamer2k6@gmail.com>
2025-10-31 15:14:49 +0700

    Add test_git.java

---
+++ test_git.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package practice_code_hrs;
+
+import java.util.Objects;
+
+/**
+ *
+ * @author My PC
+ */
+public class Customer {
+    private String customerId;
+    private String fullName;
+    private String email;
+    private String phoneNumber;
+
+    public Customer(String customerId, String fullname, String email, String phoneNumber) {
+        this.customerId = customerId;
+        this.fullName = fullname;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+    }
+    
+    // Getters and Setters
+    public String getCustomerId() { return customerId; }
+    public void setCustomerId(String customerId) { this.customerId = customerId; }
+    public String getFullName() { return fullName; }
+    public void setFullName(String fullName) { this.fullName = fullName; }
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+    public String getPhoneNumber() { return phoneNumber; }
+    public void setPhoneNumber(String phoneNumber) { this.phoneNumber = phoneNumber; }
+    
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "customerId='" + customerId + '\'' +
+                ", fullName='" + fullName + '\'' +
+                ", email='" + email + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                '}';
+    }
+    
+    public String toFileString() {
+        return customerId + "|" + fullName + "|" + email + "|" + phoneNumber;
+    }
+    
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Customer customer = (Customer) o;
+        return Objects.equals(customerId, customer.customerId);
+    }
+}
